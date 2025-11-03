f7906c4dac80b8a7d37ba7a5ded0dc3c35af1c15
Zaeem Imtiaz <zaeemimtiaz904@gmail.com>
2025-11-04 01:18:04 +0500

    feat: Add Supabase integration and environment configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:dev_flow/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dev_flow/presentation/views/splash/splash.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -15,6 +18,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Supabase.initialize(
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      url: dotenv.env['SUPABASE_URL']!,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(  // Make sure this is GetMaterialApp, not MaterialApp
